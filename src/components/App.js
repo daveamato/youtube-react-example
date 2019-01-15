1ec561bc82ed4931eeefa95b73d4d8a2c67d1968
MD Shakir Hussain <MDShakirH@hcl.com>
2019-01-15 11:52:38 +0530

    First youtube app

---
+++ src/components/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import SearchBar from './SearchBar';
+import youtube from '../apis/youtubeApi';
+import VideoList from './VideoList';
+import VideoDetails from './VideoDetails';
+
+class App extends React.Component {
+    state = { videos: [], selectedVideo: null };
+    componentDidMount() {
+        this.onQuerySubmit('buildings')
+    }
+    // Search for query
+    onQuerySubmit = async (query)=> {
+        const response = await youtube.get('/search', {
+            params: {
+                q: query
+            }
+        });
+        
+        this.setState({ 
+            videos: response.data.items,
+            selectedVideo: response.data.items[0]
+        })
+    };
+
+    onVideoSelect = video => {
+        this.setState({ selectedVideo: video });
+    };
+
+    render() {
+        return (
+            <div className="container youtube-layout">
+                <SearchBar onFormSubmit= {this.onQuerySubmit}/>
+                <VideoDetails video={this.state.selectedVideo}/>
+                <VideoList 
+                    videos={this.state.videos}
+                    onVideoSelect={this.onVideoSelect}
+                />
+            </div>
+        )
+    }
+}
+
+export default App;
